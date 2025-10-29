52af219dd69d8a6d830d08e029b9ef7bd116709f
ChaosOfPersia <dzq2301@gmail.com>
2025-10-29 18:37:53 +0100

    Update class.php

--- src/class.php
+++ src/class.php
@@ -56,4 +56,84 @@ class Paciente
 
 }
 
+<?php
+    class registro{
+        private $usuario;
+        public $pass;
+        public $pass2;
+        private function __construct($usuario_,$pass_,$pass2_) {
+            $registro->usuario=$usuario_;
+            $registro->pass=$pass_;
+            $registro->pass2=$pass2_;
+        }
+        function comprobarPass(){
+            if($registro->pass != $registro->pass2){
+                while(True){
+                    $salida='<html>
+                             <head>
+                             <meta charset="UTF-8">
+                                <!--<link rel="stylesheet" href="css/styles.css">-->
+                                <title></title>
+                             </head>
+                             <body>
+                                 <form class="registro" action="resgistrarUsuario.php" method="post">
+                                     <h1>Login</h1>
+                                     <label>Usuario</label>
+                                     <div>
+                                        <input type="text" name="nombre" value="" placeholder="Username"/>
+                                     </div>
+                                         <label>Contraseña</label>
+                                     <div>
+                                         <input type="password" name="pass" value="" placeholder="Password"/>
+                                     </div>
+                                    
+                                     <label>Repite la contraseña</label>                                    
+                                     <div>
+                                        <input type="password" name="pass2" value="" placeholder="Password"/>
+                                     </div>';
+                    return $salida;
+                }
+            }
+        }
+    }
+
+    class registro{
+        private $usuario;
+        public $pass;
+        public $pass2;
+        private function __construct($usuario_,$pass_,$pass2_) {
+            $registro->usuario=$usuario_;
+            $registro->pass=$pass_;
+            $registro->pass2=$pass2_;
+        }
+        function comprobarPass(){
+            if($registro->pass != $registro->pass2){
+                while(True){
+                    $salida='<html>
+                             <head>
+                             <meta charset="UTF-8">
+                                <!--<link rel="stylesheet" href="css/styles.css">-->
+                                <title></title>
+                             </head>
+                             <body>
+                                 <form class="registro" action="resgistrarUsuario.php" method="post">
+                                     <h1>Login</h1>
+                                     <label>Usuario</label>
+                                     <div>
+                                        <input type="text" name="nombre" value="" placeholder="Username"/>
+                                     </div>
+                                         <label>Contraseña</label>
+                                     <div>
+                                         <input type="password" name="pass" value="" placeholder="Password"/>
+                                     </div>
+                                    
+                                     <label>Repite la contraseña</label>                                    
+                                     <div>
+                                        <input type="password" name="pass2" value="" placeholder="Password"/>
+                                     </div>';
+                    return $salida;
+                }
+            }
+        }
+    }
 ?>
