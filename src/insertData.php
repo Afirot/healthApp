1e8c010fff391fe6df57f720cd225f851353e321
Ignacio Sánchez Pérez <153087902+Afirot@users.noreply.github.com>
2025-10-29 18:37:48 +0100

    src: Inclusion de clases
    He actualizado insertData para incluir el uso de clases en su funcionamiento.

--- src/insertData.php
+++ src/insertData.php
@@ -3,42 +3,21 @@ session_start();
 
 try {
     include 'functions.php';
+    include 'class.php';
 
     $conexion = db_conection('localhost', 'db_users', "wdwBSz4uwFblFQ2C", 'health_app');
     if (!isset($_SESSION['islogged']) || !$_SESSION['islogged'] === true) {
         header('Location: index.php');
         exit;
     }
-    $idUsuario = $_SESSION['userid'];
-
-    $consulta = 'SELECT username FROM users WHERE userid = :id LIMIT 1';
-    $resultado = $conexion->prepare($consulta);
-    $resultado->bindParam(':id', $idUsuario);
-    $resultado->execute();
-    $conexion = '';
+    $paciente = new Paciente();
 } catch (Throwable $ex) {
     error_log("Error: " . $ex->getMessage());
     header('Location: error500.php');
     exit();
 }
 
-$altura = $_POST['altura'];
-$peso = $_POST['peso'];
-
-$fecha = date('Y-m-d');
-
-$conexion = db_conection('localhost', 'inserter_user', "UuPZONibjAC0fJgj", 'health_app');
-
-$consulta = 'INSERT INTO `datos` (`userid`, `altura`, `peso`, `fecha`) VALUES (:userid, :altura, :peso, :fecha);';
-
-$resultado = $conexion->prepare($consulta);
-
-$resultado->bindParam(':userid', $idUsuario);
-$resultado->bindParam(':altura', $altura);
-$resultado->bindParam(':peso', $peso);
-$resultado->bindParam(':fecha', $fecha);
-
-$resultado->execute();
+$paciente->send_data($_POST['peso'], $_POST['altura']);
 
 header('Location: main.php');
 exit();
