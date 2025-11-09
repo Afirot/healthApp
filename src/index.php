d153604610740d0db10c8eb2db910c5e3de9fcfb
ChaosOfPersia <dzq2301@gmail.com>
2025-11-09 23:56:53 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 try {
     session_start();
     if (isset($_SESSION['islogged'])) {
-        header('location: main.php');
+        header('location: home.php');
     }
 } catch (Throwable $ex) {
     error_log("Error: " . $ex->getMessage());
@@ -14,24 +14,34 @@ try {
 <html>
     <head>
         <meta charset="UTF-8">
-        <!--<link rel="stylesheet" href="css/styles.css">-->
+        <link rel="stylesheet" href="css/paneles.css">
         <title></title>
     </head>
     <body>
-        <form class="login" action="login.php" method="post">
-            <h1>Login</h1>
-            <label>Usuario</label>
-            <div>
-                 <input type="text" name="nombre" value="" placeholder="Username"/>
+        <div class="login-container">
+            <form class="login" action="login.php" method="post">
+                <div class="logo-text">
+                    health<span class="highlight">-app</span>
+                </div>
+                <h1>Login</h1>
+                <label>Usuario</label>
+                <div>
+                     <input type="text" name="nombre" value=""
+                            placeholder="Username"/>
+                </div>
+                <label>Contraseña</label>
+                <div>
+                    <input type="password" name="pass" value=""
+                           placeholder="Password"/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <hr>
+            <div class="register-link">
+                <form class="registro" action="panelRegistro.php" method="post">
+                    <button type="submit">Haz click para registrarte</button>
+                </form>
             </div>
-            <label>Contraseña</label>
-            <div>
-                <input type="password" name="pass" value="" placeholder="Password"/>
-            </div>
-            <button type="submit">Login</button>
-        </form>
-        <form class="registro" action="panelRegistro.php" method="post">
-            <button type="submit">Haz click para registrarte</button>
-        </form>
+        </div>
     </body>
 </html>
