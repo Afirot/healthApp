ff95fe650ceaea408dbbf2a8eb416d8696441a19
ChaosOfPersia <dzq2301@gmail.com>
2025-11-09 23:51:18 +0100

    Update panelRegistro.php

--- src/panelRegistro.php
+++ src/panelRegistro.php
@@ -9,42 +9,73 @@ try {
     header('Location: error500.php');
 }
 ?>
-<!DOCTYPE html>
 
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <!--<link rel="stylesheet" href="css/styles.css">-->
+        <link rel="stylesheet" href="css/paneles.css">
         <title></title>
     </head>
     <body>
-        <form class="registro" action="resgistrarUsuario.php" method="post">
+        <form class="registro" action="registrarUsuario.php" method="post">
             <h1>Registro</h1>
+            <br>
             <label>Usuario</label>
             <div>
-                 <input type="text" name="usuario" value="" placeholder="Username"/>
+                <input type="text" name="usuario" id="usuario" value=""
+                       title="El nombre del usuario debe contener solo letras 
+                              y números"
+                       pattern="[A-Za-z0-9]{2,32}"
+                       required/>
             </div>
+            <br>
             <label>Nombre</label>
             <div>
-                 <input type="text" name="nombre" value="" placeholder="Name"/>
+                 <input type="text" name="nombre" id="nombre" value=""
+                        title="El nombre debe contener solo letras"
+                        pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ]{2,32}" title="" required/>
             </div>
+            <br>
             <label>Apellidos</label>
             <div>
-                 <input type="text" name="apellidos" value="" placeholder="Surname"/>
+                 <input type="text" name="apellidos" id="apellidos" value=""
+                        title="Los apellidos deben contener solo letras"
+                        pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{5,32}"
+                        required/>
             </div>
-            <label>Nombre</label>
+            <br>
+            <label>Fecha de Nacimiento</label>
             <div>
-                 <input type="text" name="nombre" value="" placeholder="Name"/>
+                 <input type="date" name="fechaNacimiento" id="fechaNacimiento"
+                        value=""
+                        title="La fecha de nacimiento del paciente es
+                               obligatorio"
+                        required/>
             </div>
+            <br>
             <label>Contraseña</label>
             <div>
-                <input type="password" name="pass" value="" placeholder="Password"/>
+                <input type="password" name="pass" id="pass"
+                       pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$"
+                       value=""
+                       title="La contraseña debe contener mínimo 8 caracteres,
+                              al menos una letra mayúscula y un caracter
+                              especial"
+                       required/>
             </div>
+            <br>
             <label>Repite la contraseña</label>
             <div>
-                <input type="password" name="pass2" value="" placeholder="Password"/>
+                <input type="password" name="pass2" id="pass2"
+                       pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$"
+                       value=""
+                       title="Debe introducir la contraseña de nuevo
+                              obligatoriamente"
+                       required/>
             </div>
-            <button type="submit">Registrarse</button>
+            <br>
+            <button type="submit"><a>Registrarse<a/></button>
         </form>
     </body>
 </html>
