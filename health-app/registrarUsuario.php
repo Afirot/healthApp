a5130787b910fd8b56c002c94b68e64d40d8cbf3
Ignacio Sánchez Pérez <153087902+Afirot@users.noreply.github.com>
2025-11-15 17:01:00 +0100

    Delete health-app directory

--- health-app/registrarUsuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'class.php';
-    
-    $registro = new registro($_POST['usuario'], $_POST['nombre'],
-                             $_POST['apellidos'], $_POST['fechaNacimiento'],
-                             $_POST['pass'], $_POST['pass2']);
-    
-    $registro->comprobarPass();
