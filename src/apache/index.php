9045102802d091c0961251934a65b42b061b62ef
ChaosOfPersia <dzq2301@gmail.com>
2026-02-10 12:42:31 +0100

    Update index.php
    Implementación de reCAPTCHA V3

--- src/apache/index.php
+++ src/apache/index.php
@@ -10,38 +10,39 @@ try {
 }
 ?>
 <!DOCTYPE html>
-
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/paneles.css">
-        <title></title>
-    </head>
-    <body>
-        <div class="login-container">
-            <form class="login" action="login.php" method="post">
-                <div class="logo-text">
-                    health<span class="highlight">-app</span>
-                </div>
-                <h1>Login</h1>
-                <label>Usuario</label>
-                <div>
-                     <input type="text" name="nombre" value=""
-                            placeholder="Username"/>
-                </div>
-                <label>Contraseña</label>
-                <div>
-                    <input type="password" name="pass" value=""
-                           placeholder="Password"/>
-                </div>
-                <button type="submit">Login</button>
-            </form>
-            <hr>
-            <div class="register-link">
-                <form class="registro" action="panelRegistro.php" method="post">
-                    <button type="submit">Haz click para registrarte</button>
-                </form>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/paneles.css">
+    <title>Login - Health App</title>
+    <script src="https://www.google.com/recaptcha/api.js?render=6LfaqWYsAAAAAB6-VarlZVgzz9bj31BLiUe7w6fh"></script>
+    <script src="recaptcha.js" defer></script>
+</head>
+<body>
+    <div class="login-container">
+        <form class="login" onsubmit="onClick(event)" action="login.php" method="post">
+            <div class="logo-text">
+                health<span class="highlight">-app</span>
             </div>
+            <h1>Login</h1>
+            <label>Usuario</label>
+            <div>
+                 <input type="text" name="nombre" placeholder="Username" required />
+            </div>
+            <label>Contraseña</label>
+            <div>
+                <input type="password" name="pass" placeholder="Password" required />
+            </div>
+            <!-- Input oculto para el token reCAPTCHA -->
+            <input type="hidden" name="token" id="recaptchaToken" />
+            <button type="submit">Login</button>
+        </form>
+        <hr>
+        <div class="register-link">
+            <form class="registro" action="panelRegistro.php" method="post">
+                <button type="submit">Haz click para registrarte</button>
+            </form>
         </div>
-    </body>
+    </div>
+</body>
 </html>
