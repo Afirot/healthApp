6098d847ecf7767a342f2f5a12965b5e5b60da22
Ignacio Sánchez Pérez <153087902+Afirot@users.noreply.github.com>
2026-02-23 16:55:29 +0100

    Update datos_usuario.php

--- src/apache/api/datos_usuario.php
+++ src/apache/api/datos_usuario.php
@@ -5,8 +5,8 @@ header("Strict-Transport-Security: max-age=63072000; includeSubDomains; preload"
 header("Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; connect-src 'self'; frame-ancestors 'none';");
 
 try {
-    include 'functions.php';
-    include "class.php";
+    include '../functions.php';
+    include "../class.php";
     
     $conexion = db_conection('127.0.0.1', 'db_users', "wdwBSz4uwFblFQ2C", 'health_app');
     if (!isset($_SESSION['jwt'])) {
@@ -34,6 +34,7 @@ header('Content-Type: application/json');
 echo json_encode($datos);
 
 ?>
+
 
 
 
